0ccc5cc2622c680f32123b28eb5aefa6d5a985fe
BrunoNicanor <88382684+BrunoNicanor@users.noreply.github.com>
2022-11-22 06:52:17 -0300

    Add files via upload

---
+++ Atividade12/script.js
@@ -0,0 +1,4 @@
+var janelaAberta = '<img src = "../../Img/janelaaberta.png">';
+var janelaFechada = '<img src = "../../Img/janelafechada.png">';
+var janelaQuebrada = '<img src = "../../Img/janelaquebra.png">';
+
